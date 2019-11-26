0832e11de5d7072bacb1cd54c3727e90310813b0
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2019-11-26 10:53:03 -0200

    create canvaDraw component

---
+++ src/app/canvas-draw/canvas-draw.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas-draw',
+  templateUrl: './canvas-draw.component.html',
+  styleUrls: ['./canvas-draw.component.scss'],
+})
+export class CanvasDrawComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
